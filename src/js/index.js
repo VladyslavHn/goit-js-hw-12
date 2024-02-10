2417081c1b8003d31ddc6366023c45d82cf533d9
VladyslavHn <vlad.gnatok@gmail.com>
2024-02-10 04:29:52 +0100

    markup

--- src/js/index.js
+++ src/js/index.js
@@ -2,22 +2,28 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from "axios";
 
 const form = document.querySelector('.search-form');
 const list = document.querySelector('.pictures-list');
 const loader = document.querySelector('.loader');
+const loadBtn = document.querySelector('.button-load');
 
 const lightbox = new SimpleLightbox('.gallery-card a.gallary-card-link', {
-            captionsData: 'alt',
-            captionDelay: 250,
-        });
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+let search = null;
+let totalResult = 0;
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
-    const searchImg = e.target.elements.input.value;
-    
-    if (searchImg.trim('') === '') {
+    let searchImg = e.target.elements.input.value;
+    search = searchImg;
+    page = 1;
+
+    if (search.trim('') === '') {
         return;
     }
 
@@ -29,8 +35,9 @@ form.addEventListener('submit', (e) => {
     }
 
     setTimeout(() => {
-        getImg(searchImg)
+        getImg()
             .then(data => {
+                totalResult = data.totalHits;
                 render(data.hits);
                 lightbox.refresh();
                 if (data.hits.length === 0) {
@@ -39,8 +46,12 @@ form.addEventListener('submit', (e) => {
                         position: 'topRight',
                         backgroundColor: 'red',
                         messageColor: 'white',
-                    })
+                    });
                 }
+                
+                loadBtn.style.opacity = 1;
+                loadBtn.style.overflow = 'visible';
+                
             })
             .catch(error => {
                 console.error('Помилка отримання зображень:', error);
@@ -49,28 +60,64 @@ form.addEventListener('submit', (e) => {
                 loader.style.display = 'none';
             });
     }, 500);
-    
+    btnChange();
     e.target.reset();
 });
 
-function getImg(searchImg) {
+let page = 1;
+let perPage = 15;
+
+loadBtn.addEventListener('click', async () => {
+    page += 1;
+    const data = await getImg();
+    render(data.hits);
+    lightbox.refresh();
+    btnChange();
+    
+    const galleryCards = document.querySelectorAll('.gallery-card');
+    galleryCards.forEach(card => {
+        const cardSize = card.getBoundingClientRect();
+        window.scrollBy({
+            top: cardSize.height * 1.36,
+            behavior: "smooth",
+        });
+    });
+});
+
+
+
+
+async function getImg() {
     const API_KEY = '42193842-675e74ed987999787d4b57f5e';
-    const URL = 'https://pixabay.com/api/';
-    const PARAMS = new URLSearchParams({
+    
+    const params = new URLSearchParams({
         key: API_KEY,
-        q: searchImg,
+        per_page: perPage,
+        page: page,
+        q: search,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
     });
+    
+    const response = await axios.get(`https://pixabay.com/api/?${params}`);
+    return response.data;
+}
 
-    return fetch(`${URL}?${PARAMS}`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
+function btnChange() {
+    const maxPage = Math.ceil(totalResult / perPage);
+    if (page === maxPage) {
+        iziToast.warning({
+                        message: "We're sorry, but you've reached the end of search results.",
+                        position: 'topRight',
+                        backgroundColor: '#add8e6',
+                        messageColor: 'white',
         });
+        loadBtn.disabled = true;
+        loadBtn.style.opacity = 0;
+        loadBtn.style.overflow = hidden;
+    }
+
 }
 
 function render(imgs) {
@@ -102,5 +149,3 @@ function render(imgs) {
 
     list.insertAdjacentHTML('beforeend', markup);
 }
-
-
